46a914cdffb9174489867813844d8146848cfd56
Kolamin <gofranich@gmail.com>
2022-10-10 12:50:07 +0600

    login action

--- my-blog/src/store/index.js
+++ my-blog/src/store/index.js
@@ -3,7 +3,8 @@ import  { createStore } from 'vuex'
 //firebase imports
 import { auth } from '@/firebase/config'
 import {
-    createUserWithEmailAndPassword
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
 } from 'firebase/auth'
 
 const store = createStore({
@@ -26,6 +27,17 @@ const store = createStore({
                 context.commit('setUser', res.user)
             } else {
                 throw new Error('could not complete signup')
+            }
+        },
+        async login(context, { email, password }) {
+            console.log('login action')
+
+            //async code
+            const res = await signInWithEmailAndPassword(auth, email, password)
+            if (res){
+                context.commit('setUser', res.user)
+            } else {
+                throw new Error('could not complete login')
             }
         }
     }
